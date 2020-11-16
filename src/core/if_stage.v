8c4fbd3aafedef04d0696c792bd22fc5da63a6a4
virusxiewy <virusxiewy99@gmail.com>
2020-11-16 14:46:35 +0800

    commit

--- src/core/if_stage.v
+++ src/core/if_stage.v
@@ -2,10 +2,8 @@ module if_stage (
     input wire clk_i;
     input wire rst_ni;
 
-    input wire boot_i;
-    input wire [INSTR_ADDR-1:0] boot_addr_i;
-    input wire [INSTR_ADDR-1:0] instr_addr;
-    output wire[INSTR_WITDH-1:0] fetch_instr;
+    input wire [31:0] instr_addr;
+    output wire[31:0] fetch_instr;
 );
     
 endmodule
---
+++ src/core/pc_reg.v
@@ -0,0 +1,29 @@
+module pc_reg (
+    input wire clk_i;
+    input wire rst_ni;
+
+    input wire reboot_i;
+    input wire [31:0] boot_addr_i;
+    input wire jump_flag;
+    input wire [31:0] jump_addr_i;
+    input wire [`Hold_Flag_Bus]hold_flag_i;
+    output wire[31:0] addr;
+
+
+);
+    reg [31:0]pc;
+    
+    always @(posedge clk ) begin
+        if (rst_ni == 1'b0 || reboot_i == 1'b1) begin
+            pc <= boot_addr_i;
+        end else if (jump_flag == 1'b1) begin
+            pc <= jump_addr_i;
+        end else if  (hold_flag_i >= `hold_PC) begin
+            pc <= pc;
+        end else begin
+            pc <= pc + 32'h4;
+        end 
+    end 
+    assign addr = pc;
+
+endmodule
