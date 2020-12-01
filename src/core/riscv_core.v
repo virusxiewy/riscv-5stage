21242da44a1d03ef9f362de01b9543c5f766d770
virusxiewy <virusxiewy99@gmail.com>
2020-12-01 17:46:39 +0800

    commit

--- src/core/riscv_core.v
+++ src/core/riscv_core.v
@@ -1,14 +1,35 @@
 /*
 *Copyright 2020 wy, virusxiewy99@live.cn
 *
-*regfile.v - regfile in riscv-5stage cpu
+*risv_core.v - top modole of the core
 * 
 */
 
 module riscv_core (
-    input wire clk_i;
-    input wire rst_ni
+    input wire clk_i,
+    input wire rst_ni,
+
+    input wire[31:0] boot_addr 
 );
+    ctrl ctrl_i(.clk_i(clk_i),
+                .rst_ni(rst_ni));
+
+    frontend froutend_i(.clk_i(clk_i),
+                        .rst_ni(rst_ni));
+
+    if_stage if_stage_i(.clk_i(clk_i),
+                        .rst_ni(rst_ni));
+
+    id_stage id_stage_i(.clk_i(clk_i),
+                        .rst_ni(rst_ni),
+                        );
+    
+    ex_stage ex_stage_i(.clk_i(clk_i),
+                        .rst_ni(rst_ni));
+
+    wb_stage wb_stage_i(.clk_i(clk_i),
+                        .rst_ni(rst_ni));
+
     
 
 
